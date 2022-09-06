42476a1401c0e5ec777c03e48af48cdaf2592099
seokkyu <tjrrb95@gmail.com>
2022-09-07 02:52:43 +0900

    doing(): pessimistic lock time out exception 만들기

--- src/test/java/me/screw/javademostudy/lock/LockTestApplication.java
+++ src/test/java/me/screw/javademostudy/lock/LockTestApplication.java
@@ -5,38 +5,67 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.persistence.*;
+import javax.transaction.Transactional;
 
 public class LockTestApplication {
 
-//    @PersistenceContext(unitName = "bankUnit")
-    private EntityManager entityManager;
-
-    @BeforeEach
-    public void setUp() {
-        System.out.println(getClass().getResource("META-INF/persistence.xml"));
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("bankUnit");
-        entityManager = entityManagerFactory.createEntityManager();
-    }
-
-    @AfterEach
-    public void release() {
-        entityManager.close();
-    }
-
     @Test
     public void test() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("bankUnit");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
         EntityTransaction transaction = entityManager.getTransaction();
         transaction.begin();
 
         Bank bank = new Bank();
-        bank.setCategory("DEPOSIT");
-        bank.setName("SHINHAN");
+        bank.setCategory("DEPOSIT2");
+        bank.setName("SHINHAN2");
 
         entityManager.persist(bank);
 
         transaction.commit();
+        entityManager.close();
     }
 
+    @Test
+    public void LockTimeOutExceptionTest() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("bankUnit");
+        EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+        EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+        new Thread(() -> {
+            findBank(entityManager1, 1L);
+        }).start();
 
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        new Thread(() -> {
+            findBank(entityManager2, 1L);
+        }).start();
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Bank findBank(EntityManager entityManager, Long id) {
+        System.out.println(Thread.currentThread().getId() + " thread start");
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        Bank bank = entityManager.find(Bank.class, id, LockModeType.PESSIMISTIC_WRITE);
+        try {
+            Thread.sleep(3_000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getId() + " thread end");
+        transaction.commit();
+        entityManager.close();
+        return bank;
+    }
 
 }

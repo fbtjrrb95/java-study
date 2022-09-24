d0179df93bcee7a281aaa3e5e1cb20c45bbc4327
seokkyu <tjrrb95@gmail.com>
2022-09-25 03:29:54 +0900

    feat(): simulate when two transaction try to obtain pessimitic locks concurrently with no wait

--- src/test/java/me/screw/javademostudy/lock/LockTestApplication.java
+++ src/test/java/me/screw/javademostudy/lock/LockTestApplication.java
@@ -24,9 +24,7 @@ public class LockTestApplication {
         entityManager.close();
     }
 
-    /**
-     * 정말? mysql jpa 는 pessimistic-write option 을 무시한다고?
-     */
+
     @Test
     public void QueryTimeOutExceptionTest() {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("bankUnit");
@@ -34,12 +32,12 @@ public class LockTestApplication {
         EntityManager entityManager2 = entityManagerFactory.createEntityManager();
 
         new Thread(() -> {
-            System.out.println(findBank(entityManager1, 1L));
+            System.out.println(findBank(entityManager1, 1L, LockModeType.PESSIMISTIC_WRITE, false));
             entityManager1.close();
         }).start();
 
         new Thread(() -> {
-            System.out.println(findBank(entityManager2, 1L));
+            System.out.println(findBank(entityManager2, 1L, LockModeType.PESSIMISTIC_WRITE, false));
             entityManager2.close();
         }).start();
 
@@ -50,15 +48,49 @@ public class LockTestApplication {
         }
     }
 
-    public Bank findBank(EntityManager entityManager, Long id) {
+    /**
+     * hibernate mysql dialect 에 따라 같은 경우에도 다른 이름의 exception 이 생길 수 있다.
+     * mysql8 dialect 와 mysql57 dialect 일때 다르다.
+     */
+    @Test
+    public void NoWaitExceptionTest() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("bankUnit");
+        EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+        EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+
+        new Thread(() -> {
+            System.out.println(findBank(entityManager1, 1L, LockModeType.PESSIMISTIC_WRITE, true));
+            entityManager1.close();
+        }).start();
+
+        new Thread(() -> {
+            System.out.println(findBank(entityManager2, 1L, LockModeType.PESSIMISTIC_WRITE, true));
+            entityManager2.close();
+        }).start();
+
+        try {
+            Thread.sleep(10_000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Bank findBank(
+                EntityManager entityManager,
+                Long id,
+                LockModeType lockModeType,
+                boolean isNoWait) {
         EntityTransaction transaction = entityManager.getTransaction();
         System.out.println(transaction + " transaction start");
         transaction.begin();
-        List<Bank> resultList = entityManager.createQuery("select b from Bank b where b.id = ?1")
+        Query query = entityManager.createQuery("select b from Bank b where b.id = ?1")
                 .setParameter(1, id)
                 .setHint("javax.persistence.query.timeout", "2000")
-                .setLockMode(LockModeType.PESSIMISTIC_WRITE)
-                .getResultList();
+                .setLockMode(lockModeType);
+        if (isNoWait) {
+            query.setHint("javax.persistence.lock.timeout", "0");
+        }
+        List<Bank> resultList = query.getResultList();
         try {
             Thread.sleep(3_000);
         } catch (InterruptedException e) {
@@ -66,7 +98,9 @@ public class LockTestApplication {
         }
         System.out.println(transaction + " transaction end");
         transaction.commit();
-        if (resultList == null || resultList.isEmpty()) return null;
+        if (resultList == null || resultList.isEmpty()) {
+            return null;
+        }
         return resultList.get(0);
     }
 

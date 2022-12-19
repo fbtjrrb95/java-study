f3e3648062eb8c766dccfa75d35fd35381ec8556
seokkyu <tjrrb95@gmail.com>
2022-12-20 00:51:02 +0900

    feat(): modify add test code

--- src/test/java/me/screw/javademostudy/datastructure/linkedlist/LinkedListTest.java
+++ src/test/java/me/screw/javademostudy/datastructure/linkedlist/LinkedListTest.java
@@ -1,8 +1,9 @@
 package me.screw.javademostudy.datastructure.linkedlist;
 
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LinkedListTest {
 
@@ -10,11 +11,16 @@ class LinkedListTest {
     public void add() {
         LinkedList linkedList = new LinkedList();
         ListNode listNode = ListNode.builder().number(1).build();
-        linkedList.add(linkedList.getHead(), listNode, 0);
-        ListNode listNode2 = ListNode.builder().number(2).build();
-        linkedList.add(linkedList.getHead(), listNode2, 0);
+        ListNode nodeAtPosition0 = linkedList.add(linkedList.getHead(), listNode, 0);
 
-        linkedList.print(linkedList.getHead());
+        ListNode listNode2 = ListNode.builder().number(2).build();
+        ListNode nodeAtPosition1 = linkedList.add(linkedList.getHead(), listNode2, 1);
+
+        assertThat(nodeAtPosition1.getNext()).isNull();
+        assertThat(nodeAtPosition1.getNumber()).isEqualTo(2);
+
+        assertThat(nodeAtPosition0.getNext()).isEqualTo(nodeAtPosition1);
+        assertThat(nodeAtPosition0.getNumber()).isEqualTo(1);
     }
 
     @Test

29118584db6cf1df98609f61c5ce0a46ed46fe89
seokkyu <tjrrb95@gmail.com>
2022-12-31 02:15:45 +0900

    feat(): add test code linked list when given invalid postion to add

--- src/test/java/me/screw/javademostudy/datastructure/linkedlist/LinkedListTest.java
+++ src/test/java/me/screw/javademostudy/datastructure/linkedlist/LinkedListTest.java
@@ -3,7 +3,7 @@ package me.screw.javademostudy.datastructure.linkedlist;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 class LinkedListTest {
 
@@ -24,10 +24,23 @@ class LinkedListTest {
     }
 
     @Test
+    public void add_invalid_position() {
+        LinkedList linkedList = new LinkedList();
+        ListNode listNode = ListNode.builder().number(1).build();
+        linkedList.add(linkedList.getHead(), listNode, 0);
+
+        ListNode listNode2 = ListNode.builder().number(2).build();
+        int invalidPosition = 2;
+        assertThatRuntimeException().isThrownBy(() -> {
+            linkedList.add(linkedList.getHead(), listNode2, invalidPosition);
+        });
+    }
+
+    @Test
     void remove_맨끝노드_삭제했을경우() {
         LinkedList linkedList = new LinkedList();
         ListNode listNode = ListNode.builder().number(1).build();
-        ListNode nodeAtPosition0 = linkedList.add(linkedList.getHead(), listNode, 0);
+        linkedList.add(linkedList.getHead(), listNode, 0);
 
         ListNode listNode2 = ListNode.builder().number(2).build();
         ListNode nodeAtPosition1 = linkedList.add(linkedList.getHead(), listNode2, 1);
@@ -45,13 +58,13 @@ class LinkedListTest {
     void remove_맨처음노드_삭제했을경우() {
         LinkedList linkedList = new LinkedList();
         ListNode listNode = ListNode.builder().number(1).build();
-        ListNode nodeAtPosition0 = linkedList.add(linkedList.getHead(), listNode, 0);
+        linkedList.add(linkedList.getHead(), listNode, 0);
 
         ListNode listNode2 = ListNode.builder().number(2).build();
         ListNode nodeAtPosition1 = linkedList.add(linkedList.getHead(), listNode2, 1);
 
         ListNode listNode3 = ListNode.builder().number(3).build();
-        ListNode nodeAtPosition2 = linkedList.add(linkedList.getHead(), listNode3, 2);
+        linkedList.add(linkedList.getHead(), listNode3, 2);
 
         ListNode removedNode = linkedList.remove(linkedList.getHead(), 0);
 

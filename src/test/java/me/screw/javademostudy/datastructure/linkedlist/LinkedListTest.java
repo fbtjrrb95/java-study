2af31faa371e0eea74e7a56eacdc2d481d44b37a
seokkyu <tjrrb95@gmail.com>
2023-01-02 01:21:41 +0900

    feat(): linked list add & modify test code

--- src/test/java/me/screw/javademostudy/datastructure/linkedlist/LinkedListTest.java
+++ src/test/java/me/screw/javademostudy/datastructure/linkedlist/LinkedListTest.java
@@ -8,6 +8,20 @@ import static org.assertj.core.api.Assertions.*;
 class LinkedListTest {
 
     @Test
+    public void getNode() {
+        LinkedList linkedList = new LinkedList();
+        ListNode listNode1 = ListNode.builder().number(1).build();
+        linkedList.add(linkedList.getHead(), listNode1, 0);
+
+        assertThat(linkedList.getNode(linkedList.getHead(), 0)).isEqualTo(listNode1);
+
+        ListNode listNode2 = ListNode.builder().number(2).build();
+        linkedList.add(linkedList.getHead(), listNode2, 1);
+
+        assertThat(linkedList.getNode(linkedList.getHead(), 1)).isEqualTo(listNode2);
+    }
+
+    @Test
     public void add() {
         LinkedList linkedList = new LinkedList();
         ListNode listNode = ListNode.builder().number(1).build();
@@ -46,12 +60,13 @@ class LinkedListTest {
         ListNode nodeAtPosition1 = linkedList.add(linkedList.getHead(), listNode2, 1);
 
         ListNode listNode3 = ListNode.builder().number(3).build();
-        ListNode nodeAtPosition2 = linkedList.add(linkedList.getHead(), listNode3, 2);
+        linkedList.add(linkedList.getHead(), listNode3, 2);
 
         ListNode removedNode = linkedList.remove(linkedList.getHead(), 2);
 
-        assertThat(removedNode.getNumber()).isEqualTo(2);
-        assertThat(nodeAtPosition1.getNext()).isEqualTo(nodeAtPosition2);
+        assertThat(removedNode.getNumber()).isEqualTo(3);
+        assertThat(nodeAtPosition1.getNext()).isNull();
+        assertThat(removedNode.getNext()).isNull();
     }
 
     @Test

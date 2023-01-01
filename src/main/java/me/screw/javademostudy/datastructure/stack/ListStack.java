572573912120aee075a433d6132bebbe6952b0dd
seokkyu <tjrrb95@gmail.com>
2023-01-02 01:22:07 +0900

    feat(): list stack modify push logic

--- src/main/java/me/screw/javademostudy/datastructure/stack/ListStack.java
+++ src/main/java/me/screw/javademostudy/datastructure/stack/ListStack.java
@@ -7,13 +7,14 @@ import me.screw.javademostudy.datastructure.linkedlist.ListNode;
 
 @RequiredArgsConstructor
 public class ListStack {
+    @Getter
     private final LinkedList list;
     @Getter
     private int size;
 
     public void push(int data) {
         ListNode listNode = ListNode.builder().number(data).build();
-        list.add(list.getHead(), listNode, size - 1);
+        list.add(list.getHead(), listNode, size);
         size++;
     }
 

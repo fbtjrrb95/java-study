15cf9ae04deeb9b5769f7f61842c637906078936
seokkyu <tjrrb95@gmail.com>
2023-01-04 01:29:37 +0900

    feat(): apply atomic integer at list stack size for race condition

--- src/main/java/me/screw/javademostudy/datastructure/stack/ListStack.java
+++ src/main/java/me/screw/javademostudy/datastructure/stack/ListStack.java
@@ -5,22 +5,29 @@ import lombok.RequiredArgsConstructor;
 import me.screw.javademostudy.datastructure.linkedlist.LinkedList;
 import me.screw.javademostudy.datastructure.linkedlist.ListNode;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 @RequiredArgsConstructor
 public class ListStack {
     @Getter
     private final LinkedList list;
-    @Getter
-    private int size;
+
+    private final AtomicInteger size = new AtomicInteger(0);
+
+    public int getSize() {
+        return size.get();
+    }
 
     public void push(int data) {
         ListNode listNode = ListNode.builder().number(data).build();
-        list.add(list.getHead(), listNode, size);
-        size++;
+        list.add(list.getHead(), listNode, size.get());
+        size.set(size.get() + 1);
     }
 
     public int pop() {
-        ListNode removedNode = list.remove(list.getHead(), size - 1);
-        size--;
+        if (size.get() <= 0) return -1;
+        ListNode removedNode = list.remove(list.getHead(), size.get() - 1);
+        size.set(size.get() - 1);
         return removedNode.getNumber();
     }
 }

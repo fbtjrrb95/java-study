aafddac283d7b6dcede1b513e459d684d3cc2b54
seokkyu <tjrrb95@gmail.com>
2023-01-08 21:16:45 +0900

    rollback(): rollback modifying type of size int to autointeger

--- src/main/java/me/screw/javademostudy/datastructure/stack/ListStack.java
+++ src/main/java/me/screw/javademostudy/datastructure/stack/ListStack.java
@@ -5,29 +5,22 @@ import lombok.RequiredArgsConstructor;
 import me.screw.javademostudy.datastructure.linkedlist.LinkedList;
 import me.screw.javademostudy.datastructure.linkedlist.ListNode;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 @RequiredArgsConstructor
 public class ListStack {
     @Getter
     private final LinkedList list;
 
-    private final AtomicInteger size = new AtomicInteger(0);
-
-    public int getSize() {
-        return size.get();
-    }
+    @Getter
+    private int size = 0;
 
     public void push(int data) {
         ListNode listNode = ListNode.builder().number(data).build();
-        list.add(list.getHead(), listNode, size.get());
-        size.set(size.get() + 1);
+        list.add(list.getHead(), listNode, size++);
     }
 
     public int pop() {
-        if (size.get() <= 0) return -1;
-        ListNode removedNode = list.remove(list.getHead(), size.get() - 1);
-        size.set(size.get() - 1);
+        if (size <= 0) return -1;
+        ListNode removedNode = list.remove(list.getHead(), --size);
         return removedNode.getNumber();
     }
 }

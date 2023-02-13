9608b5d02273447d8185e770313027e7679eab7e
seokkyu <tjrrb95@gmail.com>
2023-02-14 00:28:13 +0900

    feat(binary-tree): implement node class

--- src/main/java/me/screw/javademostudy/datastructure/binarytree/Tree.java
+++ src/main/java/me/screw/javademostudy/datastructure/binarytree/Tree.java
@@ -1,4 +1,12 @@
 package me.screw.javademostudy.datastructure.binarytree;
 
+import java.util.Optional;
+
 public interface Tree {
+
+    Optional<Node> getRoot();
+
+    Optional<Node> getChild(Node node, int i);
+
+    void addChild(Node parent, int i);
 }

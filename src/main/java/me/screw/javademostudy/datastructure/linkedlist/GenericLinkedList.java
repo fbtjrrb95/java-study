0c151b02089431b64956f6cf49e5c0791944d888
seokkyu <tjrrb95@gmail.com>
2023-02-21 00:02:19 +0900

    feat(binary-tree): create linked list with generic type

---
+++ src/main/java/me/screw/javademostudy/datastructure/linkedlist/GenericLinkedList.java
@@ -0,0 +1,59 @@
+package me.screw.javademostudy.datastructure.linkedlist;
+
+import lombok.Getter;
+
+public class GenericLinkedList<T> {
+
+    @Getter
+    private final GenericListNode<T> head = new GenericListNode<>(null);
+
+    public T add(T valueToAdd, int position) {
+        if (position < 0) {
+            throw new IndexOutOfBoundsException("position must be positive");
+        }
+
+        GenericListNode<T> node = getNode(head, position - 1);
+        GenericListNode<T> nodeToAdd = new GenericListNode<>(valueToAdd);
+        nodeToAdd.setNext(node.getNext());
+        node.setNext(nodeToAdd);
+        return nodeToAdd.getItem();
+    }
+
+    public T remove(int positionToRemove) {
+        if (positionToRemove < 0) {
+            throw new IndexOutOfBoundsException("position must be positive");
+        }
+
+        GenericListNode<T> prevNodeToRemove = head;
+        if (positionToRemove - 1 >= 0) {
+            prevNodeToRemove = getNode(head, positionToRemove - 1);
+        }
+        GenericListNode<T> nodeToRemove = prevNodeToRemove.getNext();
+        prevNodeToRemove.setNext(nodeToRemove.getNext());
+        nodeToRemove.setNext(null);
+        return nodeToRemove.getItem();
+    }
+
+    public boolean contains(ListNode head, ListNode nodeToCheck) {
+        ListNode curNode = head;
+        while (curNode.getNext() != null) {
+            if (curNode.getNext() == nodeToCheck) return true;
+
+            curNode = curNode.getNext();
+        }
+        return false;
+    }
+
+    private GenericListNode<T> getNode(GenericListNode<T> head, int position) {
+        GenericListNode<T> targetNode = head;
+        while (position-- >= 0) {
+            if (targetNode.getNext() == null) {
+                throw new RuntimeException("invalid position");
+            }
+
+            targetNode = targetNode.getNext();
+        }
+        return targetNode;
+    }
+
+}

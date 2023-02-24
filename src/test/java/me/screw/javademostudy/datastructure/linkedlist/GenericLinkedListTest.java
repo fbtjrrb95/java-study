c0ef808e989e621f8d3cd9e21d13c47bac034804
seokkyu <tjrrb95@gmail.com>
2023-02-24 22:55:22 +0900

    feat(binary-tree): add test code when add integer invalid position

--- src/test/java/me/screw/javademostudy/datastructure/linkedlist/GenericLinkedListTest.java
+++ src/test/java/me/screw/javademostudy/datastructure/linkedlist/GenericLinkedListTest.java
@@ -1,19 +1,35 @@
 package me.screw.javademostudy.datastructure.linkedlist;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatRuntimeException;
 
 public class GenericLinkedListTest {
 
+    private GenericLinkedList<Integer> linkedList;
+
+    @BeforeEach
+    void setUp() {
+        linkedList = new GenericLinkedList<>();
+    }
+
     @Test
     void add() {
-        GenericLinkedList<Integer> linkedList = new GenericLinkedList<>();
-
         Integer valueAtPosition0 = linkedList.add(1, 0);
         Integer valueAtPosition1 = linkedList.add(2, 1);
 
         assertThat(valueAtPosition0).isEqualTo(1);
         assertThat(valueAtPosition1).isEqualTo(2);
+    }
+
+    @Test
+    void add_invalid_position() {
+        Integer valueAtPosition0 = linkedList.add(1, 0);
+        int invalidPosition = 2;
+        assertThatRuntimeException().isThrownBy(() -> {
+            linkedList.add(2, invalidPosition);
+        });
     }
 }

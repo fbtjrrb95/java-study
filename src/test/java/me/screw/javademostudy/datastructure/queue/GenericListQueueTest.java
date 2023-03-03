873810f561ac14791d4b131c66aab10b4fa2a160
seokkyu <tjrrb95@gmail.com>
2023-03-03 23:11:38 +0900

    feat(): add generic list queue test code

---
+++ src/test/java/me/screw/javademostudy/datastructure/queue/GenericListQueueTest.java
@@ -0,0 +1,30 @@
+package me.screw.javademostudy.datastructure.queue;
+
+import me.screw.javademostudy.datastructure.linkedlist.GenericLinkedList;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GenericListQueueTest {
+
+    private GenericListQueue<Integer> queue;
+
+    @BeforeEach
+    void setUp() {
+        GenericLinkedList<Integer> list = new GenericLinkedList<>();
+        queue = new GenericListQueue<>(list);
+    }
+
+    @Test
+    void pushAndPop() {
+        queue.push(1);
+        Assertions.assertThat(queue.getSize()).isEqualTo(1);
+
+        queue.push(2);
+        Assertions.assertThat(queue.getSize()).isEqualTo(2);
+
+        Assertions.assertThat(queue.pop()).isEqualTo(1);
+        Assertions.assertThat(queue.getSize()).isEqualTo(1);
+        Assertions.assertThat(queue.pop()).isEqualTo(2);
+    }
+}

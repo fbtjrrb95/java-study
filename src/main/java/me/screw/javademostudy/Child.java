5d41f465cbabdbe71bb63b96e0ee3ff989546a9a
seokkyu <tjrrb95@gmail.com>
2023-05-29 01:30:28 +0900

    temp(): add some codes about inheritance

---
+++ src/main/java/me/screw/javademostudy/Child.java
@@ -0,0 +1,9 @@
+package me.screw.javademostudy;
+
+public class Child extends Parent {
+
+    @Override
+    protected void print() {
+        System.out.println("I am child");
+    }
+}
--- src/main/java/me/screw/javademostudy/JavaDemoStudyApplication.java
+++ src/main/java/me/screw/javademostudy/JavaDemoStudyApplication.java
@@ -2,8 +2,15 @@ package me.screw.javademostudy;
 
 public class JavaDemoStudyApplication {
 
-    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+    public static void main(String[] args) {
 
+        Parent parent = new Parent();
+        Parent child = new Child();
+        Child child1 = new Child();
+
+        parent.print();
+        child.print(); // static method dispatch
+        child1.print(); // dynamic method dispatch
     }
 
 }

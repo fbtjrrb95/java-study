c9ee69ca1f013cd4f87a334b9bc5b9e151b33fc2
seokkyu <tjrrb95@gmail.com>
2024-04-10 23:22:14 +0900

    feat(): add race condition example codes

--- src/main/java/me/screw/javademostudy/thread/Car.java
+++ src/main/java/me/screw/javademostudy/thread/Car.java
@@ -1,6 +1,20 @@
 package me.screw.javademostudy.thread;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import static me.screw.javademostudy.thread.ThreadTestApplication.flag;
+
+@RequiredArgsConstructor
 public class Car extends Vehicle implements Runnable {
+
+    @Getter
+    private final String carName;
+
+    @Override
+    public void drive() {
+        flag.increase();
+    }
 
     @Override
     public void run() {

6a05293b3b8cfe1431d59461e30592b51065b42e
seokkyu <tjrrb95@gmail.com>
2024-05-14 10:13:11 +0900

    refactor(): apply modified package name

--- src/main/java/me/skrew/javastudy/file/FileReaderTestApplication.java
+++ src/main/java/me/skrew/javastudy/file/FileReaderTestApplication.java
@@ -6,7 +6,7 @@ public class FileReaderTestApplication {
 
     public static void main(String[] args) {
        FileReader fileReader = new FileReader();
-        File file = new File("java-demo-study/src/main/java/me/screw/javademostudy/file/test.xlsx");
+        File file = new File("src/main/java/me/skrew/javastudy/file/test.xlsx");
        fileReader.read(file);
     }
 
